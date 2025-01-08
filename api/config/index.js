450ad70bf5e57c7aba89311286805d17fa992527
Onur Avan <avanonur@hotmail.com>
2025-01-08 19:21:59 +0300

    config settings set

--- api/config/index.js
+++ api/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  LOG_LEVEL: process.env.LOG_LEVEL || "debug",
-  CONNECTION_STRING:
-    process.env.CONNECTION_STRING || "mongodb://localhost:27017/node-base",
+  "PORT": process.env.PORT || 3000,
+  "LOG_LEVEL": process.env.LOG_LEVEL || "debug",
+  "CONNECTION_STRING": process.env.CONNECTION_STRING || "mongodb://localhost:27017/node-base",
 };
