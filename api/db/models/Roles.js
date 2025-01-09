afd0a2d796931d060944f9d58305f0eabdf4a901
Onur Avan <avanonur@hotmail.com>
2025-01-10 02:15:28 +0300

    roles models change

--- api/db/models/Roles.js
+++ api/db/models/Roles.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const RolePrivileges = require("./RolePrivileges");
 
 const schema = mongoose.Schema(
   {
-    role_name: { type: String, required: true },
+    role_name: { type: String, required: true, unique: true },
     is_active: { type: Boolean, default: true },
     created_by: {
       type: mongoose.SchemaTypes.ObjectId,
@@ -17,7 +18,15 @@ const schema = mongoose.Schema(
   }
 );
 
-class Roles extends mongoose.model {}
+class Roles extends mongoose.model {
+ static async deleteMany(query) {
+    if (query._id) {
+      await RolePrivileges.deleteMany({ role_id: query._id });
+    }
+
+    await super.deleteMany(query);
+  }
+}
 
 schema.loadClass(Roles);
 module.exports = mongoose.model("roles", schema);
