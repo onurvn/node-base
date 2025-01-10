eb7177225b6de91c47db6734cbeb8ae124ce94c1
Onur Avan <avanonur@hotmail.com>
2025-01-11 01:32:43 +0300

    users model update

--- api/db/models/Users.js
+++ api/db/models/Users.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const schema = mongoose.Schema(
   {
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     is_active: { type: Boolean, default: true },
     first_name: String,
@@ -10,6 +10,7 @@ const schema = mongoose.Schema(
     phone_number: String,
   },
   {
+    versionKey: false,
     timestamps: {
       createdAt: "created_at",
       updatedAt: "updated_at",
@@ -17,7 +18,7 @@ const schema = mongoose.Schema(
   }
 );
 
-class Users extends mongoose.model {}
+class Users extends mongoose.model { }
 
 schema.loadClass(Users);
 module.exports = mongoose.model("users", schema);
