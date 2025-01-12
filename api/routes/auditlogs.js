5703177f7e17da3ede6a147cee8babb34f07b09f
Onur Avan <avanonur@hotmail.com>
2025-01-12 15:46:33 +0300

    auditlogs routes created

--- api/routes/auditlogs.js
+++ api/routes/auditlogs.js
@@ -1,13 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const moment = require("moment")
 
-router.get("/:id", (req, res, next) => {
-  res.json({
-    body: req.body,
-    params: req.params,
-    query: req.query,
-    headers: req.headers,
-  });
+const Response = require("../lib/Response");
+const AuditLogs = require("../db/models/AuditLogs");
+
+router.post("/", async (req, res) => {
+  let body = req.body;
+  let query = {};
+  let skip = body.skip;
+  let limit = body.limit;
+
+  try {
+    if (typeof body.skip !== "numeric") {
+      skip = 0;
+    }
+
+    if (typeof body.limit !== "numeric" || body.limit > 500) {
+      limit = 500;
+    }
+
+    let auditLogs = await AuditLogs.find(query).sort({ created_at: -1 }).skip(skip).limit(limit);
+
+    if (body.begin_date && body.end_date) {
+      query.created_at = {
+        $gte: moment(body.begin_date),
+        $lte: moment(body.end_date)
+      }
+    } else {
+      query.created_at = {
+        $gte: moment().subtract(1, "day").startOf("day"),
+        $lte: moment()
+      }
+    }
+
+
+    res.json(Response.successResponse(auditLogs));
+
+
+  } catch (error) {
+    let errorResponse = Response.errorResponse(error);
+    res.status(errorResponse.code).json(Response.errorResponse(error));
+  }
 });
 
 module.exports = router;
